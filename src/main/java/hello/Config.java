f6ea4ad3bce29f94bbdbc99c60b44545c9184759
zhangchao <passion888168@126.com>
2019-09-21 12:49:11 +0800

    web init

---
+++ src/main/java/hello/Config.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@EnableRedisHttpSession
+public class Config {
+
+	@Bean
+	public LettuceConnectionFactory connectionFactory() {
+		return new LettuceConnectionFactory("127.0.0.1", 6379);
+	}
+}
